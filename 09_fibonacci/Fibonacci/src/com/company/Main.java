1f3bf71fce0bc293f9cc0211e86b34a834f87825
Unknown <josu.mtnz@gmail.com>
2017-11-20 20:48:52 +0100

    Ejercicio 09 terminado.

--- 09_fibonacci/Fibonacci/src/com/company/Main.java
+++ 09_fibonacci/Fibonacci/src/com/company/Main.java
@@ -18,7 +18,7 @@ public class Main {
         int a = 0;
         int b = 1;
         int c;
-        for (int i = 0; i == n; i++) {
+        for (int i = 0; i < n; i++) {
             c = b + a;
             a = b;
             b = c;
